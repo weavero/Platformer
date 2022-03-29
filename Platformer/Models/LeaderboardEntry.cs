c17338fda5fde195d24f15d8ecaaf8ff6c3b7642
weavero <weaveroliver16@gmail.com>
2022-03-29 16:20:42 +0200

    Added database

---
+++ Platformer/Models/LeaderboardEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Platformer.Models
+{
+    public partial class LeaderboardEntry
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public string Time { get; set; }
+    }
+}
